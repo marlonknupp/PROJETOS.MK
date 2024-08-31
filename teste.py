c75aaf1b48eebd0eccfee768a0d4807e5331be44
Marlon Knupp <marlonjcc23@gmail.com>
2024-08-31 19:50:31 -0300

    updating

---
+++ teste.py
@@ -0,0 +1,7 @@
+c = int(input('Digite um numero para ve sua tabuada'))
+for num in range(1,11):
+    print (f'{c}x{num}={c*num}')
+
+print ('------------------')
+print ('-------- PARABENS -----------')
+print ('---------------------------')
