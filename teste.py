7aafb6bd679a88cbe44d619995d96e1f86de11dd
Marlon Knupp <marlonjcc23@gmail.com>
2024-10-02 23:07:38 -0300

    ...

--- teste.py
+++ teste.py
@@ -1,15 +1,1 @@
-from newsapi import NewsApiClient
-
-newsapi = NewsApiClient(api_key='SUA_APIKEY')
-
-all_articles = newsapi.get_everything(
-    q='bitcoin',
-    soucers= 'globo, info-money',
-    domains='globo.com, infomoney.com.br',
-    from_param='2024-08-01',
-    to='2024-09-21',
-    language='pt',                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-    sort_by='publishedAt',
-)
-
-print(all_articles)
+###
