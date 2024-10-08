96ad67d1a285fc9bb53cc6c2df3bf782e5e4ddb2
Marlon Knupp <marlonjcc23@gmail.com>
2024-10-07 21:15:23 -0300

    ...

--- funçao_classe.py
+++ funçao_classe.py
@@ -5,17 +5,17 @@ class Celular:
     bateria = 'infinita'
     tem_camera = 'False'
 
-def fazer_ligaçao(self):
-   print('fazendo ligaçao....')
+    def fazer_ligaçao(self):
+     print('fazendo ligaçao....')
 
-def jogando_cobrinha(self):
-   print('jogando conbrinha....')
+    def jogando_cobrinha(self):
+     print('jogando conbrinha....')
 
-def despertador(self):
-   print ('despertando...')
+    def despertador(self):
+     print ('despertando...')
 
-def calcular(self, v1, v2):
-    return v1 + v2
+    def calcular(self, v1, v2):
+      return v1 + v2
 
 celular = Celular()
 calculado = celular.calcular(2,4)
